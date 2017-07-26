104f45260305769ace0232155ae49924d3d21373
Jase Cutler <jasecutler@gmail.com>
2017-07-26 16:07:00 -0700

    Initial page setup

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express()
 const path = require('path')
 const mustacheExpress = require('mustache-express');
 
+let todos = {todos:[
+  {task:"Clean the car",todoid:1},
+  {task:"Clean the garage",todoid:2},
+  {task:"Make a todo list",todoid:3,status:"complete"}
+  ]}
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
@@ -10,7 +15,7 @@ app.set('view engine', 'mustache')
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.get("/", function(req, res, next){
-  res.render("index", {appType:"Express"})
+  res.render("index", todos)
 })
 
 app.listen(3000, function(){
