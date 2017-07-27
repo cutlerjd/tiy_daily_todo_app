f9cb6981294a2ad9c11e1883f3ad25b64ba4ac65
Jase Cutler <jasecutler@gmail.com>
2017-07-26 17:13:09 -0700

    Submit now sends todos and redirects to home page

--- app.js
+++ app.js
@@ -2,18 +2,28 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser')
 
+//Sample todo items.
 let todos = {todos:[
   {task:"Clean the car",todoid:1},
   {task:"Clean the garage",todoid:2},
   {task:"Make a todo list",todoid:3,status:"complete"}
   ]}
+
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname, 'static')))
 
+app.post("/new",function(req,res,next){
+  let todo = {task: req.body.newtodo,
+              todoid: (todos.todos.length + 1)}
+    todos.todos.push(todo)
+    res.redirect("/")
+})
 app.get("/", function(req, res, next){
   res.render("index", todos)
 })
