4181a965bc711472d2878052baf88d117c9146be
Jase Cutler <jasecutler@gmail.com>
2017-07-26 20:56:01 -0700

    App now remembers state of todos

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express()
 const path = require('path')
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
 
 //Sample todo items.
 let todos = {todos:[
   {task:"Clean the car",todoid:1},
-  {task:"Clean the garage",todoid:2},
+  {task:"Clean the garage",todoid:2,status:"complete"},
   {task:"Make a todo list",todoid:3,status:"complete"}
   ]}
 
@@ -17,12 +18,28 @@ app.set('view engine', 'mustache')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname, 'static')))
+app.use(expressValidator())
 
-app.post("/new",function(req,res,next){
+app.post("/update",function(req,res,next){
+  req.checkBody("newtodo","Todo is empty.").isLength(1,100)
+  let errors = req.validationErrors()
+  if(errors){
+    console.log(errors)
+    if(Array.isArray(req.body.todo)){
+      updateTodos(req.body.todo)
+    }else{
+      let arr = []
+      arr.push(req.body.todo)
+      updateTodos(arr)
+    }
+    res.redirect("/")
+  } else { 
   let todo = {task: req.body.newtodo,
               todoid: (todos.todos.length + 1)}
     todos.todos.push(todo)
+    console.log(req.body.todo)
     res.redirect("/")
+  }
 })
 app.get("/", function(req, res, next){
   res.render("index", todos)
@@ -31,3 +48,18 @@ app.get("/", function(req, res, next){
 app.listen(3000, function(){
   console.log("App running on port 3000")
 })
+
+function updateTodos(arr){
+  console.log(todos)
+  console.log(todos.todos)
+  todos.todos.forEach(function(item){
+    let checks = arr.filter(function(el){
+      return (el == item.todoid)
+    })
+    if(checks > 0){
+      item.status = "complete"
+    }else {
+      item.status = null
+    }
+  })
+}
